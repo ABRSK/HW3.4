82848dc63f18789d9079285fb2a25bec4e438ba4
AreyBRSK <areybrsk@gmail.com>
2022-06-13 16:00:07 +0300

    App view (not finished)

--- HW3.4/ContentView.swift
+++ HW3.4/ContentView.swift
@@ -7,10 +7,39 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var targetValue = Int.random(in: 0...100)
+    @State private var currentValue = 50.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Get as close a possible to: \(targetValue)")
+            HStack {
+                Text("  0")
+                Slider(value: $currentValue, in: 0...100, step: 1)
+                    .tint(.green)
+                Text("100")
+            }
+            UIKitSlider(sliderValue: $currentValue, opacity: computeScore())
+            Text("\(currentValue)")
+            Text("\(computeScore())")
+            Color(.green)
+                .frame(width: 100, height: 100)
+                .opacity(Double(computeScore()) / 100)
+            Button("Get Score") {
+                // Alert with result
+            }
+            Button("New Value") {
+                targetValue = Int.random(in: 0...100)
+            }
+        }
+        .padding()
+    }
+    
+    private func computeScore() -> Int {
+        let difference = abs(targetValue - lround(currentValue))
+        return 100 - difference
     }
 }
 
