00bc58cb60e52534dab0fd3cc1d6c0eb681d43f9
AreyBRSK <areybrsk@gmail.com>
2022-06-13 14:02:17 +0300

    Initial Commit

---
+++ HW3.4/HW3_4App.swift
@@ -0,0 +1,17 @@
+//
+//  HW3_4App.swift
+//  HW3.4
+//
+//  Created by Андрей Барсук on 13.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW3_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
